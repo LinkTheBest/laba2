eb2267fa70a0edb759a5ac5797de7d6fb084f7ce
nickyking <nikita29042001@mail.ru>
2019-10-01 19:29:53 +0300

    Final Version

--- src/AncientPower.java
+++ src/AncientPower.java
@@ -5,14 +5,18 @@ public class AncientPower extends SpecialMove{
         super(Type.ROCK, 60,  100);
     }
 
-    protected void applyOppEffects(Pokemon pokemon) {
+
+    @Override
+    protected void applySelfEffects(Pokemon pokemon) {
         if (Math.random() <= 0.1){
-            pokemon.setMod(Stat.ATTACK,  6);
-            pokemon.setMod(Stat.DEFENSE,  6);
-            pokemon.setMod(Stat.SPECIAL_ATTACK, 6);
-            pokemon.setMod(Stat.SPECIAL_DEFENSE, 6);
-            pokemon.setMod(Stat.SPEED, 6);
+            pokemon.setMod(Stat.ATTACK,  1);
+            pokemon.setMod(Stat.DEFENSE,  1);
+            pokemon.setMod(Stat.SPECIAL_ATTACK, 1);
+            pokemon.setMod(Stat.SPECIAL_DEFENSE, 1);
+            pokemon.setMod(Stat.SPEED, 1);
 
         }
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+         Battle battle_field = new Battle();
+         battle_field.addAlly(new Lugia("Первый", 2));
+         battle_field.addAlly(new Gulpin("Второй", 2));
+         battle_field.addAlly(new Swalot("Третий", 2));
+         battle_field.addFoe(new Bounsweet("Четвертый", 2));
+         battle_field.addFoe(new Steenee("Пятый", 2));
+         battle_field.addFoe(new Tsareena("Шестой", 2));
+         battle_field.go();
+
+
 
     }
 }
